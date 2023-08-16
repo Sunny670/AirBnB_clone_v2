21ec7e58551693d02647467e1b1905fb6013ffa9
Sunny670 <sunny.thanki13@gmail.com>
2023-08-16 16:27:29 +0000

    updates

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Script that starts the Flask web application"""
 
-from flask import Flask
+from flask import Flask, rendre_template
 
 app = Flask(__name__)
 
@@ -9,7 +9,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+    return render_template("9-states.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=None)
