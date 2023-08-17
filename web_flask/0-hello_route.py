5f47ace977d0a046d86df51f7b383a57b8185c67
Sunny670 <sunny.thanki13@gmail.com>
2023-08-17 17:39:34 +0000

    updates

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Script that starts the Flask web application"""
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
 
-from flask import Flask, rendre_template
+from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
-
-@app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
-    return render_template("9-states.html")
+@app.route('/')
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_hbnb():
+    """Return a given string"""
+    return ("Hello HBNB!")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000)
